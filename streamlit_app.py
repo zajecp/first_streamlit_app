035958299f9b9b99af4727c0f9db3b97fd9ec26d
Przemysław Ziętal <113998778+zajecp@users.noreply.github.com>
2022-09-21 09:06:25 +0200

    dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit
+import pandas
 
 streamlit.header('🥣Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal🥑')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg🐔')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(my_fruit_list)
