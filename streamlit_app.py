1b72652e0d7488be9cd2701a379a93b847cd507a
Przemysław Ziętal <113998778+zajecp@users.noreply.github.com>
2022-09-21 09:03:51 +0200

    immage

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.header('🥣Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal🥑')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Hard-Boiled Free-Range Egg🐔')
