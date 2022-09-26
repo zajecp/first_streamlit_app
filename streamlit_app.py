c0f76dd132aaaaa8c7954a5f3e6ac1ae64bd541f
Przemysław Ziętal <113998778+zajecp@users.noreply.github.com>
2022-09-26 10:02:49 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,8 +31,8 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text("The Fruit Load List")
-streamlit.text(my_data_row)
+streamlit.header("The Fruit Load List")
+streamlit.dataframe(my_data_row)
 
 
 
